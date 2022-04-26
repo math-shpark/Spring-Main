64a602b40a7bf02f103919bf9f4e097b898ce47d
Su-ho Park <psuhoo122065@gmail.com>
2022-04-27 00:32:31 +0900

    Updated on Apr 27 2022 00:32

---
+++ springboothello1/src/main/java/com/ssafy/hello/Springboothello1Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboothello1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboothello1Application.class, args);
+	}
+
+}
---
+++ springboothello1/src/test/java/com/ssafy/hello/Springboothello1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboothello1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
